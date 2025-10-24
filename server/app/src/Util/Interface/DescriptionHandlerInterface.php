9a31a0feebdb32f883ff89a35d7aa82a209a1f00
Vitalii Khomiakov <vitalii.khomiakov@yandex.ru>
2025-10-24 23:32:41 +0200

    restructure project

--- server/app/src/Util/Interface/DescriptionHandlerInterface.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Util\Interface;
-
-interface DescriptionHandlerInterface
-{
-    public function processObsceneWords(array $obsceneWords, string $text): string;
-}
